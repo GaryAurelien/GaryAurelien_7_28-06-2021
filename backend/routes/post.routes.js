dc0dda20feb3b47fe9d0606e3c2d24ff89d6de50
Aurélien <aurelien_gary@hotmail.fr>
2021-07-13 15:49:25 +0200

    mise en place middleware

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const postCtrl = require('../controllers/post.controllers.js');
 
+const postCtrl = require('../controllers/post.controllers.js');
+const auth = require('../middleware/auth');
 
 /*const verifyPassword = require('../middleware/verifyPassword')*/
 
-router.post('/create', /* verifyPassword, */ postCtrl.create);
-router.put('/:postId',/*auth,*/ postCtrl.update);
-router.delete('/:postId',/*auth,*/postCtrl.delete);
-router.get('/:id', /*auth,*/ postCtrl.findOne);
-router.get('/', /*auth,*/ postCtrl.findAll);
+router.post('/create', auth, postCtrl.create);
+router.put('/:postId', auth, postCtrl.update);
+router.delete('/:postId', auth, postCtrl.delete);
+router.get('/:id', auth, postCtrl.findOne);
+router.get('/', auth, postCtrl.findAll);
 //router.post('/:id/like', /*auth,*/ saucesCtrl.likeOrDislikeSauce);
 
 module.exports = router;
