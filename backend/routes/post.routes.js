67dc9ac8c6eb70543a40abde9f2d02db64a94550
Aurélien <aurelien_gary@hotmail.fr>
2021-07-15 11:36:26 +0200

    frontend init + mise en place de sass

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -6,11 +6,11 @@ const auth = require('../middleware/auth');
 
 /*const verifyPassword = require('../middleware/verifyPassword')*/
 
-router.post('/create', auth, postCtrl.create);
-router.put('/:postId', auth, postCtrl.update);
-router.delete('/:postId', auth, postCtrl.delete);
-router.get('/:id', auth, postCtrl.findOne);
-router.get('/', auth, postCtrl.findAll);
+router.post('/create', /*auth,*/ postCtrl.create);
+router.put('/:postId', /*auth,*/ postCtrl.update);
+router.delete('/:postId', /*auth,*/ postCtrl.delete);
+router.get('/:id', /*auth,*/ postCtrl.findOne);
+router.get('/', /*auth,*/ postCtrl.findAll);
 //router.post('/:id/like', /*auth,*/ saucesCtrl.likeOrDislikeSauce);
 
 module.exports = router;
