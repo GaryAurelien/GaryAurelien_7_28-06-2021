e6896a6c0343559e084154ac69190068451be85b
Aurélien <aurelien_gary@hotmail.fr>
2021-08-04 09:00:46 +0200

    mise en place de meta title

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,11 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Post from '../views/Post.vue'
+import NotFound from '../views/NotFound.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta: {
+      title: 'Acceuil'
+    }
   },
   { 
     path: '/Login',
@@ -13,38 +18,51 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue'),
+    meta: {
+      title: 'Connection'
+    }
   },
   {
     path: '/Signup',
     name: 'Signup',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Signup.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/Signup.vue'),
+    meta: {
+      title: 'CréationDeCompte'
+    }
   },
   {
     path: '/Post',
     name: 'Post',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Post.vue')
+    component: Post ,
+    meta: {
+      title: 'PagePosts'
+    }
   },
   {
     path: '/create',
     name: 'create',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/CreatePost.vue')
+  },
+  {
+    name: '/NotFound',
+    path: '/:pathMatch(.*)',
+    component: NotFound,
+    meta: {
+      title:'404 Not Found'
+    }
   }
 ]
 
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory(),
   routes
+})
+
+
+router.afterEach((to, from, next) => {
+  document.title = to.meta.title;
 })
 
 export default router
